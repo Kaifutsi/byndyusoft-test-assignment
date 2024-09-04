b6625eefd5f5894e00cd8a698387dc14989073a2
kaifutsi <ekaterina_94_1994@icloud.com>
2024-09-04 14:30:14 +0300

    Initial commit

---
+++ cypress/e2e/contact-info.cy.js
@@ -0,0 +1,25 @@
+describe('Проверка контактной информации на сайте Byndyusoft', () => {
+  it('Проверка ссылки на Telegram', () => {
+    cy.visit('https://www.google.ru/');
+
+    cy.get('textarea[name="q"]').type('Byndyusoft{enter}');
+
+    cy.get('body').then(($body) => {
+      if ($body.find('.mpQYc .Hg3NO .sjVJQd').length > 0) {
+        cy.get('.mpQYc .Hg3NO .sjVJQd').contains('Not now').click();
+      }
+    });
+
+    cy.get('#search a').first().then((link) => {
+      const url = link.prop('href');
+
+      cy.request(url).then((response) => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(response.body, 'text/html');
+        const telegramLink = doc.querySelector('a[href="http://t.me/alexanderbyndyu"]');
+
+        expect(telegramLink).not.to.be.null;
+      });
+    });
+  });
+});
